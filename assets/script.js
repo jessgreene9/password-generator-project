4cfd1628624443dbe04e9a530b01221b4da2ab0c
Jess Greene <jess.greene9@gmail.com>
2021-05-18 16:27:11 -0400

    fixing problems in javascript

--- assets/script.js
+++ assets/script.js
@@ -10,66 +10,73 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 
 // Write password to the #password input
 
-  
-  
+
+
 // create function for generating password
-var generatePassword= function() {
+function generatePassword() {
   var passwordLength = promptMessage();
 
-// creates prompt message when generate button is clicked
-function promptMessage() {
-  
-  
-  var length = prompt ("Please enter the desired length of your password.");
-  
-  //checks for parameters for password and returns to prompt message
-  if
-  (length > 128 || length < 8) {
-    alert ("Please choose a number between between 8 and 128.");
-    promptMessage();
+  // creates prompt message when generate button is clicked
+  function promptMessage() {
+
+
+    passwordLength = parseInt(prompt("Please enter the desired length of your password."));
+
+    //checks for parameters for password and returns to prompt message
+    if
+      (passwordLength > 128 || passwordLength< 8) {
+      alert("Please choose a number between between 8 and 128.");
+      promptMessage();
+    }
+    //goes through confirm messages for each type of character
+    else {
+      var includesNumbers = confirm("Select OK to include numbers");
+      var includesLower = confirm("Select OK to include lowercase letters");
+      var includesUpper = confirm("Select OK to include uppercase letters");
+      var includesSpecial = confirm("Select OK to include special characters");
+    }
+
+    // if none is chosen, gives alert message and goes back to original prompt
+    if (!includesNumbers && !includesLower && !includesUpper && !includesSpecial) {
+      alert("Please select at least one password character type");
+      promptMessage();
+    } 
   }
-  //goes through confirm messages for each type of character
-  else {
-  var includesNumbers = confirm("Select OK to include numbers");
-  var includesLower = confirm ("Select OK to include lowercase letters");
-  var includesUpper = confirm ("Select OK to include uppercase letters");
-  var includesSpecial = confirm ("Select OK to include special characters"); }
+
+    var password = "";
+    
+    for (var i = 0; i < passwordLength; i++){
+
+      if (includesNumbers === true && password.length < passwordLength) {
+        password += getRandom(numeric);
+       
+
+      }
+      if (includesLower === true && password.length < passwordLength) {
+        password += getRandom(lowerCase);
+      }
+
+      if (includesUpper === true && password.length < passwordLength) {
+        password += getRandom(upperCase);
+      }
+
+      if (includesSpecial === true && password.length < passwordLength) {
+        password += getRandom(special);
+      }
+      
+  }
+  return password;
   
-  // if none is chosen, gives alert message and goes back to original prompt
-  if (!includesNumbers && !includesLower && !includesUpper && !includesSpecial) {
-    alert ("Please select at least one password character type");
-    promptMessage();
-  } 
-  
-var password = '';
-
-for (i=0; i<passwordLength; i++) {
-
-if (includesNumbers===true && password.length <passwordLength) { 
-password += getRandom (numeric);
-
+    function getRandom(array) {
+      var random =Math.floor(Math.random() * array.length);
+      var value = array[random];
+      return value;
 }
-if (includesLower===true && password.length <passwordLength) { 
-  password += getRandom (lowerCase);
- }
-
- if (includesUpper===true && password.length <passwordLength) { 
-  password += getRandom (upperCase);
- }
-
- if (includesSpecial===true && password.length <passwordLength) { 
-  password += getRandom (special);
- }
-
 
 }
 
 
-} 
-return password;
 
-
-};
 
 function writePassword() {
   var password = generatePassword();
@@ -78,19 +85,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Add event listener to generate button
